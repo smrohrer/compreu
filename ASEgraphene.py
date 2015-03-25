9ab5a57d44e44d78949ddb43bfc21356773496d9
matthewkovacs <mkovacs36@gmail.com>
2015-03-24 22:31:19 -0400

    updating script

--- ASEgraphene.py
+++ ASEgraphene.py
@@ -67,32 +67,22 @@ def print_atoms(atoms):
 
 def make_orca(atoms, filename="filename.inp", charge="0", multiplicity="1", method="am1", geometry_opt=0, output="temp.out"):
     if method == "am1" and geometry_opt == 0:
-        out=''
-        parameters0= '{0}\t{1}\t{2}\n{3}'.format("%method", "method", method, "end")
-        parameters1='{0}\t{1}\t{2}\t{3}\n'.format("*", "xyz", charge, multiplicity)
-        out=out+parameters0+parameters1
-        end_of_atom_coordinates="*"
+        parameters1= '{0}\t{1}\t{2}\n\t{3}'.format("%method", "method", method, "end")
 
     elif method == "am1" and geometry_opt == 1:
-        out=''
-        parameters0= '{0}\t{1}\t{2}\t{3}\t{4}\n{5}'.format("%method", "method", method, "method", "OPT", "end")
-        parameters1='{0}\t{1}\t{2}\t{3}\n'.format("*", "xyz", charge, multiplicity)
-        out=out+parameters0+parameters1
-        end_of_atom_coordinates="*"
+        parameters1= '{0}\n\t{1}\n\t{2}\n\n{3}\n{4}\n\n{5}\n{6}\n\n'.format("%method method am1", "method OPT", "end", "%SCF MAXITER 300", "     end", "%geom MaxIter 250", "      end")
 
     elif method == "DFT" and geometry_opt== 0:
-        out=''
-        parameters0= '{0}\t{1}\n'.format("!", "DFT-Energy")
-        parameters1='{0}\t{1}\t{2}\t{3}\n'.format("*", "xyz", charge, multiplicity)
-        out=out+parameters0+parameters1
-        end_of_atom_coordinates="*"
+        parameters1= '{0}\t{1}\n'.format("!", "DFT-Energy")
 
     elif method == "DFT" and geometry_opt == 1:
-        out=''
-        parameters0= '{0}\t{1}\n'.format("!", "Good-Opt")
-        parameters1='{0}\t{1}\t{2}\t{3}\n'.format("*", "xyz", charge, multiplicity)
-        out=out+parameters0+parameters1
-        end_of_atom_coordinates="*"
+        parameters1= '{0}\t{1}\n'.format("!", "Good-Opt")
+        
+    out = ''
+    parameters0 = '{0}\n{1}\n\n'.format("%pal nprocs 2", "     end")
+    parameters2 ='{0}\t{1}\t{2}\t{3}\n'.format("*", "xyz", charge, multiplicity)
+    out=out+parameters0+parameters1+parameters2
+    end_of_atom_coordinates="*"
 
     with open(filename, 'w') as f:
         f.write(out)
@@ -271,11 +261,9 @@ def calc_edge_nitrogens(nx="1", nz="1", method="am1", optimize_geometry=0, make_
         plt.clf()
 
 def nitrogenate_all_zig_zag(nx_min, nx_max, nz_min, nz_max, method="am1", optimize_geometry=0, make_symmetric=0):
-    all_N_SCF_energy_list, all_N_HOMO_energy_list, all_N_LUMO_energy_list, all_N_horizontal_dimension_list, all_N_vertical_dimension_list, all_N_sheet_area_list = (np.array([]) for dummy_var in range(6))
-    pltxlist = [all_N_horizontal_dimension_list, all_N_vertical_dimension_list, all_N_sheet_area_list]
-    pltylist = [all_N_SCF_energy_list, all_N_HOMO_energy_list, all_N_LUMO_energy_list]
-    pltylabel_list = ["SCF Energy", "HOMO Energy", "LUMO Energy"]
-    pltxlabel_list = ["Horizontal Sheet Dimension", "Vertical Sheet Dimension", "Sheet Area"]
+    all_N_SCF_energy_list, all_N_HOMO_energy_list, all_N_LUMO_energy_list = (np.array([]) for dummy_var in xrange(3))
+    pltylist = all_N_SCF_energy_list, all_N_HOMO_energy_list, all_N_LUMO_energy_list
+    pltylabel_list = "SCF Energy", "HOMO Energy", "LUMO Energy"
     for nx in xrange(nx_min, nx_max+1, 2):
         for nz in xrange(nz_min, nz_max+1, 2):
             if make_symmetric == 1:
@@ -317,31 +305,27 @@ def nitrogenate_all_zig_zag(nx_min, nx_max, nz_min, nz_max, method="am1", optimi
             all_N_SCF_energy_list = np.append(all_N_SCF_energy_list, data.scfenergies)
             all_N_HOMO_energy_list = np.append(all_N_HOMO_energy_list, moenergies_array[data.homos])
             all_N_LUMO_energy_list = np.append(all_N_LUMO_energy_list, moenergies_array[data.homos+1])
-            all_N_vertical_dimension_list = np.append(all_N_vertical_dimension_list, nz)
-            all_N_horizontal_dimension_list = np.append(all_N_horizontal_dimension_list, nx)
-            all_N_sheet_area_list = np.append(all_N_sheet_area_list, nx*nz)
 
             with open("nitrogenated_edge_results.txt", 'a+') as e:
                 moenergies_array = data.moenergies[0]
-                e.write("##########################\n")
+                e.write("\n##########################\n")
                 e.write("Nitrogenated %dx%d sheet\n" % (nx, nz))
                 e.write("##########################\n")
                 e.write("Total SCF energy in eV:\t")
                 e.write(str(data.scfenergies))
                 e.write("\nMolecular orbital energy of HOMO in eV:\t")
                 e.write(str(moenergies_array[data.homos]))
-                e.write("\nMolecular orbital energy of LUMO in eV:\t\n\n")
+                e.write("\nMolecular orbital energy of LUMO in eV:\t")
                 e.write(str(moenergies_array[data.homos+1]))
 
-    for x in xrange(0, 3):
-        for y in xrange(0,3):
-            plt.plot(pltxlist[x], pltylist[y])
-            plt.xlabel(pltxlabel_list[x])
-            plt.ylabel(pltylabel_list[y])
-            plt.title("Nitrogenated Zig Zag with Unsaturated Nitrogens")
-            plt.savefig("unsaturated_nitrogenated_zigzag.png")
-            plt.clf()
-
+    for y in xrange(0, 3):
+        rectangles = plt.bar(np.arange(all_N_SCF_energy_list.size), pltylist[y], 0.4, alpha=0.4, color='b')
+        plt.title("%s vs Sheet Dimensions" % pltylabel_list[y])
+        plt.ylabel(pltylabel_list[y])
+        plt.xticks(index+0.2, ("3x3","3x5", "3x7", "5x3", "5x5", "7x3"))
+        plt.xlabel("Sheet Dimension")
+        plt.savefig("%s.png" % pltylabel_list[y])
+        plt.clf()
 
 
 
@@ -494,13 +478,12 @@ def gui_nitrogenate_all():
     
 
 
-nitrogenate_all_zig_zag(3,7,3,7, optimize_geometry=1, make_symmetric=1)
-#     build_param_frame(root)
-#     calc_param_frame(root)
-#     button(root).bottom_buttons()
-
-# # #button(root).test_button()
-#     root.mainloop()
+nitrogenate_all_zig_zag(7,7,5,7, optimize_geometry=1, make_symmetric=1)
+        #build_param_frame(root)
+        #calc_param_frame(root)
+        #button(root).bottom_buttons()
+                #button(root).test_button()
+        #root.mainloop()
 
 #atoms = build_sheet(3, 3, symmetry=1)
 #nitrogenate(atoms, 34)
