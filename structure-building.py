035d563c005f2e303d7b63c336ab8762f3974f88
Eric Gottlieb <Eric Gottlieb>
2016-11-29 06:54:04 -0500

    structure file mostly separated into helper functions

--- structure-building.py
+++ structure-building.py
@@ -15,6 +15,11 @@ EV_TO_KCAL = 23.0605
 H2_TOTAL_ENERGY = -634.997248712
 H_ENERGY = -13.6
 
+coBondLength = 1.36
+chBondLength = 1.09
+ohBondLength = 0.96
+ohBondAngle = 109.5 * np.pi / 180
+
 np.set_printoptions(precision=3,suppress=True)
 
 def base_ring():
@@ -26,21 +31,12 @@ def base_ring():
                              [1.88513, 2.50000, 1.42000],
                              [1.88513, 2.50000, 2.84000]])
 
-
-    
-
 def daves_super_saturate(atoms):
     pos = atoms.get_positions()
-    tree = KDTree(atoms.get_positions())
-    list_tree = list(tree.query_pairs(1.430))
-    bondedTo = [ [] for i in range(len(atoms))] 
-
-    for bond in list_tree:
-        bondedTo[bond[0]].append(bond[1])
-        bondedTo[bond[1]].append(bond[0])
+    bondedTo = build_bonded_to(pos)
 
     Zs = atoms. get_atomic_numbers()
-# figure out what needs a hydrogen atom
+    # figure out what needs a hydrogen atom
     for iatom in range(len(atoms)):
         nbonds = len( bondedTo[iatom] )
         Z = Zs[iatom]
@@ -52,7 +48,7 @@ def daves_super_saturate(atoms):
             bond1 = pos[ bondedTo[iatom][0] , : ] - r0
             bond2 = pos[ bondedTo[iatom][1],   :]  -r0
             rH = -(bond1 + bond2)
-            rH = 1.09 * rH / np.linalg.norm(rH)
+            rH = chBondLength * rH / np.linalg.norm(rH)
             atoms.append(Atom('H',  r0+rH ))
 
 def build_bonded_to(pos):
@@ -75,6 +71,110 @@ def get_edge_atoms(atoms,bondedTo):
 
     return edge_atoms
 
+def add_alcohol(atoms):
+    pos = atoms.get_positions()
+    bondedTo = build_bonded_to(pos)
+    edge_atoms = get_edge_atoms(atoms,bondedTo)
+
+    # choose a random edge atom to attach an alcohol
+    atom = random.choice(edge_atoms)
+
+    r0 = pos[atom]
+    bond1 = pos[bondedTo[atom][0]] - r0
+    bond2 = pos[bondedTo[atom][1]] - r0
+    CO_bond = -(bond1 + bond2)
+    CO_bond = coBondLength * CO_bond / np.linalg.norm(CO_bond)
+    rotation = [[np.cos(ohBondAngle), 0, np.sin(ohBondAngle)],
+                [0, 1, 0],
+                [-np.sin(ohBondAngle), 0, np.cos(ohBondAngle)]]
+    OH_bond = np.dot(-CO_bond, rotation)
+    OH_bond = ohBondLength * OH_bond / np.linalg.norm(OH_bond)
+    atoms.append(Atom('H', r0 + CO_bond + OH_bond))
+    return atoms
+
+def add_COOH(atoms):
+    pos = atoms.get_positions()
+    bondedTo = build_bonded_to(pos)
+    edge_atoms = get_edge_atoms(atoms,bondedTo)
+
+    atom = random.choice(edge_atoms)
+    r0 = pos[atom]
+    bond1 = pos[bondedTo[atom][0]] - r0
+    bond2 = pos[bondedTo[atom][1]] - r0
+    CC_bond = -(bond1 + bond2)
+    CC_bond = 1.4 * CC_bond / np.linalg.norm(CC_bond)
+    atoms.append(Atom('C', r0 + CC_bond))
+    
+    dihedralRotationUp = rotation_matrix(CC_bond, np.pi/2)
+    dihedralRotationDown = rotation_matrix(CC_bond, -1 * np.pi/2)
+
+    angle1 = 120 * 2.0 * np.pi / 360
+    rotation1 = [[np.cos(angle1), 0, np.sin(angle1)],
+                    [0, 1, 0],
+                    [-np.sin(angle1), 0, np.cos(angle1)]]
+    CO_double_bond = np.dot(-CC_bond, rotation1)
+    CO_double_bond = np.dot(CO_double_bond, dihedralRotationUp)
+    CO_double_bond = 1.2 * CO_double_bond / np.linalg.norm(CO_double_bond)
+    atoms.append(Atom('O', r0 + CC_bond + CO_double_bond))
+    angle2 = -120 * 2.0 * np.pi / 360
+    rotation2 = [[np.cos(angle2), 0, np.sin(angle2)],
+                    [0, 1, 0],
+                    [-np.sin(angle2), 0, np.cos(angle2)]]
+    CO_bond = np.dot(-CC_bond, rotation2)
+    CO_bond = np.dot(CO_bond, dihedralRotationUp)
+    CO_bond = coBondLength * CO_bond / np.linalg.norm(CO_bond)
+    atoms.append(Atom('O', r0 + CC_bond + CO_bond))
+    angle3 = 109 * 2 * np.pi / 360
+    OH_rotation_axis = np.cross(CO_bond,CO_double_bond)
+    rotation3 = rotation_matrix(OH_rotation_axis,angle3)
+    OH_bond = np.dot(-CO_bond, rotation3)
+    OH_bond = ohBondLength * OH_bond / np.linalg.norm(OH_bond)
+    atoms.append(Atom('H', r0 + CC_bond + CO_bond + OH_bond))
+    return atoms
+
+def add_pyridinic(atoms):
+    pos = atoms.get_positions()
+    bondedTo = build_bonded_to(pos)
+
+    edge_atoms = get_edge_atoms(atoms,bondedTo)
+
+    # choose a random edge atom to replace with nitrogen
+    atom = random.choice(edge_atoms)
+    Zs = atoms.get_atomic_numbers()
+    Zs[atom] = 7
+    atoms.set_atomic_numbers(Zs)
+    return atoms
+
+def add_pyrrollic(atoms):
+    pos = atoms.get_positions()
+    bondedTo = build_bonded_to(pos)
+
+    # find a random pair of edge atoms to be replaced with a single nitrogen
+    edge_atoms = get_edge_atoms(atoms,bondedTo)
+    C1 = -1
+    C2 = -1
+    while C1 < 0 or C2 < 0:
+        C1 = random.choice(edge_atoms)
+        if len(bondedTo[bondedTo[C1][0]]) == 2:
+            C2 = bondedTo[C1][0]
+            C3 = bondedTo[C1][1]
+        elif len(bondedTo[bondedTo[C1][1]]) == 2:
+            C2 = bondedTo[C1][1]
+            C3 = bondedTo[C1][0]
+    if bondedTo[C2][0]==C1:
+        C4 = bondedTo[C2][1]
+    else:
+        C4 = bondedTo[C2][0]
+    N_pos = (pos[C1] + pos[C2] + pos[C3] + pos[C4]) / 4
+    map = [False for i in range(len(atoms))]
+    map[C1] = True
+    map[C2] = True
+    atoms.pop(map.index(True))
+    map.remove(True)
+    atoms.pop(map.index(True))
+    atoms.append(Atom('N', N_pos))
+    return atoms
+    
 def build_random_ring_structure(rings):
     atoms = base_ring()
     for iring in range(0, rings-1):
@@ -106,7 +206,6 @@ def build_random_ring_structure(rings):
 
     return atoms
     
-
 def rotation_matrix(axis, theta):
     """
     Return the rotation matrix associated with counterclockwise rotation about
@@ -127,112 +226,13 @@ def random_structure(rings=1, pyrroles=0, nitrogens=1, alcohols=0, nCOOH=1):
     atoms = build_random_ring_structure(rings)
 
     for ipyrrole in range(pyrroles):
-        pos = atoms.get_positions()
-        bondedTo = build_bonded_to(pos)
-
-        # find a random pair of edge atoms to be replaced with a single nitrogen
-        Zs = atoms.get_atomic_numbers()
-        edge_atoms = []
-        for iatom, neighbors in enumerate(bondedTo):
-            if Zs[iatom] == 6 and len(neighbors) == 2:
-                if Zs[neighbors[0]] == 6 and Zs[neighbors[1]] == 6:
-                    edge_atoms.append(iatom)
-        C1 = -1
-        C2 = -1
-        while C1 < 0 or C2 < 0:
-            C1 = random.choice(edge_atoms)
-            if len(bondedTo[bondedTo[C1][0]]) == 2:
-                C2 = bondedTo[C1][0]
-                C3 = bondedTo[C1][1]
-            elif len(bondedTo[bondedTo[C1][1]]) == 2:
-                C2 = bondedTo[C1][1]
-                C3 = bondedTo[C1][0]
-        if bondedTo[C2][0]==C1:
-            C4 = bondedTo[C2][1]
-        else:
-            C4 = bondedTo[C2][0]
-        N_pos = (pos[C1] + pos[C2] + pos[C3] + pos[C4]) / 4
-        map = [False for i in range(len(atoms))]
-        map[C1] = True
-        map[C2] = True
-        atoms.pop(map.index(True))
-        map.remove(True)
-        atoms.pop(map.index(True))
-        atoms.append(Atom('N', N_pos))
-
+        atoms = add_pyrroles(atoms)
     for iN in range(nitrogens):
-        pos = atoms.get_positions()
-        bondedTo = build_bonded_to(pos)
-
-        edge_atoms = get_edge_atoms(atoms,bondedTo)
-
-        # choose a random edge atom to replace with nitrogen
-        atom = random.choice(edge_atoms)
-        Zs = atoms.get_atomic_numbers()
-        Zs[atom] = 7
-        atoms.set_atomic_numbers(Zs)
-
-    coBondLength = 1.36
-    ohBondLength = 0.96
-    ohBondAngle = 109.5 * np.pi / 180
+        atoms = add_pyridinic(atoms)
     for iO in range(alcohols):
-        pos = atoms.get_positions()
-        bondedTo = build_bonded_to(pos)
-
-        edge_atoms = get_edge_atoms(atoms,bondedTo)
-
-        # choose a random edge atom to attach an alcohol
-        atom = random.choice(edge_atoms)
-
-        r0 = pos[atom]
-        bond1 = pos[bondedTo[atom][0]] - r0
-        bond2 = pos[bondedTo[atom][1]] - r0
-        CO_bond = -(bond1 + bond2)
-        CO_bond = coBondLength * CO_bond / np.linalg.norm(CO_bond)
-        atoms.append(Atom('O', r0 + CO_bond))
-        rotation = [[np.cos(ohBondAngle), 0, np.sin(ohBondAngle)],
-                    [0, 1, 0],
-                    [-np.sin(ohBondAngle), 0, np.cos(ohBondAngle)]]
-        OH_bond = np.dot(-CO_bond, rotation)
-        OH_bond = ohBondLength * OH_bond / np.linalg.norm(OH_bond)
-        atoms.append(Atom('H', r0 + CO_bond + OH_bond))
-
+        atoms = add_alcohol(atoms)
     for iO in range(nCOOH):
-        atom = random.choice(edge_atoms)
-
-        r0 = pos[atom]
-        bond1 = pos[bondedTo[atom][0]] - r0
-        bond2 = pos[bondedTo[atom][1]] - r0
-        CC_bond = -(bond1 + bond2)
-        CC_bond = 1.4 * CC_bond / np.linalg.norm(CC_bond)
-        atoms.append(Atom('C', r0 + CC_bond))
-        
-        dihedralRotationUp = rotation_matrix(CC_bond, np.pi/2)
-        dihedralRotationDown = rotation_matrix(CC_bond, -1 * np.pi/2)
-
-        angle1 = 120 * 2.0 * np.pi / 360
-        rotation1 = [[np.cos(angle1), 0, np.sin(angle1)],
-                     [0, 1, 0],
-                     [-np.sin(angle1), 0, np.cos(angle1)]]
-        CO_double_bond = np.dot(-CC_bond, rotation1)
-        CO_double_bond = np.dot(CO_double_bond, dihedralRotationUp)
-        CO_double_bond = 1.2 * CO_double_bond / np.linalg.norm(CO_double_bond)
-        atoms.append(Atom('O', r0 + CC_bond + CO_double_bond))
-        angle2 = -120 * 2.0 * np.pi / 360
-        rotation2 = [[np.cos(angle2), 0, np.sin(angle2)],
-                     [0, 1, 0],
-                     [-np.sin(angle2), 0, np.cos(angle2)]]
-        CO_bond = np.dot(-CC_bond, rotation2)
-        CO_bond = np.dot(CO_bond, dihedralRotationUp)
-        CO_bond = 1.4 * CO_bond / np.linalg.norm(CO_bond)
-        atoms.append(Atom('O', r0 + CC_bond + CO_bond))
-        angle3 = 109 * 2 * np.pi / 360
-        OH_rotation_axis = np.cross(CO_bond,CO_double_bond)
-        rotation3 = rotation_matrix(OH_rotation_axis,angle3)
-        OH_bond = np.dot(-CO_bond, rotation3)
-        OH_bond = 0.96 * OH_bond / np.linalg.norm(OH_bond)
-        atoms.append(Atom('H', r0 + CC_bond + CO_bond + OH_bond))
-        
+        atoms = add_COOH(atoms) 
 
     daves_super_saturate(atoms)
 
